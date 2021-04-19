7e01b61752046dce6c5c1bb7ce9947bddf84ee07
mashirowang <hyperoot@gmail.com>
2021-04-19 11:23:43 +0800

    feat(plugin-coin): init

---
+++ packages/plugin-coin/src/index.ts
@@ -0,0 +1,33 @@
+import { Context, IPlugin, Message } from '@wechot/core';
+import { got } from '@wechot/utils';
+
+const WechotPluginCoin: IPlugin = {
+  priority: 100,
+
+  apply(context: Context) {
+    const handleSearchCoinPrice = async (_message: Message, symbol: string) => {
+      if (!symbol) {
+        return null;
+      }
+      const searchSymbol = `${symbol.toUpperCase()}USDT`;
+      try {
+        const result = await got
+          .get('https://api1.binance.com/api/v3/ticker/price', {
+            searchParams: {
+              symbol: searchSymbol,
+            },
+          })
+          .json<any>();
+        if (!result?.price) {
+          return null;
+        }
+        return `查询币种：${symbol.toUpperCase()}\n当前价格：${result.price.toFixed(2)}`;
+      } catch {
+        return null;
+      }
+    };
+    context.command.register('coin', handleSearchCoinPrice);
+  },
+};
+
+export default WechotPluginCoin;
